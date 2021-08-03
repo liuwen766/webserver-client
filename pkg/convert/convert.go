63116fd09749609a9655d05e627a084d3c76b5c6
liuwen2 <liuwen2@cmss.chinamobile.com>
2021-08-03 20:08:45 +0800

    web-server

---
+++ pkg/convert/convert.go
@@ -0,0 +1,17 @@
+package convert
+
+func StringPtr(s string) *string {
+	return &s
+}
+
+func Int64Ptr(i int64) *int64 {
+	return &i
+}
+
+func BoolPtr(b bool) *bool {
+	return &b
+}
+
+func IntPtr(i int) *int {
+	return &i
+}
